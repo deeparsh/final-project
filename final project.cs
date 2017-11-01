d657c7b50623e8f9ef01caeec1a0a7470458a545
Arshdeep Singh <33296298+deeparsh@users.noreply.github.com>
2017-11-01 17:05:25 -0400

    Add files via upload

---
+++ final project.cs
@@ -0,0 +1,35 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Content;
+using System;
+
+namespace App1
+{
+    [Activity(Label = "App1", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.Main);
+
+            // Get our button from the layout resource,
+            Button btnCall = FindViewById<Button>(Resource.Id.call);
+
+            btnCall.Click += delegate
+            {
+                //open dialer
+
+                var uri1 = Android.Net.Uri.Parse("tel:2892423237");
+                var intent = new Intent(Intent.ActionDial, uri1);
+                StartActivity(intent);
+
+
+            };
+
+        }
+    }
+};
+
+
